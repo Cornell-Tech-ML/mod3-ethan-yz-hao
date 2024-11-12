6b42f97f884d4ab2bb565ae40194fcd6812a381a
Ethan Hao <hao.yz.ethan@gmail.com>
2024-11-11 19:52:00 -0500

    fix relu for numba type

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -83,7 +83,7 @@ def sigmoid(x: float) -> float:
 
 def relu(x: float) -> float:
     """Return the ReLU of x."""
-    return max(0.0, x)
+    return x if x > 0 else 0.0
 
 
 def log(x: float) -> float:
