18b006a4cc17f1de7fd162fb483b5bd4c8f09a5b
Ethan Hao <hao.yz.ethan@gmail.com>
2024-11-15 15:15:16 -0500

    task 3.5

--- project/run_fast_tensor.py
+++ project/run_fast_tensor.py
@@ -30,7 +30,9 @@ class Network(minitorch.Module):
 
     def forward(self, x):
         # TODO: Implement for Task 3.5.
-        raise NotImplementedError("Need to implement for Task 3.5")
+        h1 = self.layer1.forward(x).relu()
+        h2 = self.layer2.forward(h1).relu()
+        return self.layer3.forward(h2).sigmoid()
 
 
 class Linear(minitorch.Module):
@@ -44,7 +46,9 @@ class Linear(minitorch.Module):
 
     def forward(self, x):
         # TODO: Implement for Task 3.5.
-        raise NotImplementedError("Need to implement for Task 3.5")
+        batch = x @ self.weights.value
+        bias = self.bias.value
+        return batch + bias.view(1, bias.shape[0])
 
 
 class FastTrain:
